72a9d67157f92003f4e9d7b1a3c30d0bd487f271
Bulatov Evgeniy <evgeniy.bulatov777@gmail.com>
2026-07-06 22:08:10 +0300

    Optimization app.py

--- app.py
+++ app.py
@@ -78,13 +78,29 @@ logger.info("Инициализация сервиса...")
 app = FastAPI()
 
 # Загружаем модель в память
-model = load_model(model_path="model/recommendation_model.pkl")
+model = load_model(model_path='model/recommendation_model.pkl')
 
+# Фичи для предсказания
 user_features = load_sql('''SELECT * FROM "public"."evgenij-bulatov-jta6567_user_features"''')
-
 post_features = load_sql('''SELECT * FROM "public"."evgenij-bulatov-jta6567_post_features"''')
 
+# Посты в обычном виде
 post_info = load_sql('''SELECT * FROM public.post_text_df''')
+
+# Список фичей в правильном порядке, в котором модель обучалась
+feature_columns = ['OneHot__os_iOS', 'OneHot__source_organic', 'MeanTarget__country',
+       'MeanTarget__city', 'MeanTarget__topic', 'month',
+       'day', 'hour', 'user_id',
+       'post_id', 'gender', 'age',
+       'exp_group', 'mean_tfidf_like_posts',
+       'lenght_post', 'tfidf_mean',
+       'tfidf_max', 'svd_column',
+       'rbf_centr_1', 'rbf_centr_2',
+       'rbf_centr_3', 'rbf_centr_4',
+       'rbf_centr_5', 'rbf_centr_6',
+       'rbf_centr_7', 'rbf_centr_8',
+       'rbf_centr_9', 'rbf_centr_10',
+       'favorite_topic']
 
 logger.success("Сервис успешно инициализирован")
 
@@ -102,56 +118,41 @@ def recommended_posts(user_id: int, dt: datetime, limit: int = 10) -> List[PostG
     day = int(dt.day)
     hour = int(dt.hour)
 
-    # !!!! Структура итогового датафрейма, который подаётся в модель,
-    # должна полностью совпадать со структурой, использованной на этапе обучения
-    # (одни и те же столбцы, в том же порядке) !!!
-
     # Формирование данных для рекомендации
     df = post_features.copy()
     df['user_id'] = user_id
-    df = df.merge(user_features,
-                  on='user_id',
-                  how='left'
-    )
+
+    user_row = user_features[user_features['user_id'] == user_id].iloc[0]
+    df['OneHot__os_iOS'] = user_row['OneHot__os_iOS']
+    df['OneHot__source_organic'] = user_row['OneHot__source_organic']
+    df['MeanTarget__country'] = user_row['MeanTarget__country']
+    df['MeanTarget__city'] = user_row['MeanTarget__city']
+    df['gender'] = user_row['gender']
+    df['age'] = user_row['age']
+    df['exp_group'] = user_row['exp_group']
+    df['mean_tfidf_like_posts'] = user_row['mean_tfidf_like_posts']
+
     df['favorite_topic'] = 0
+
     df['month'] = month
     df['day'] = day
     df['hour'] = hour
 
     # Формирование данных в правильный порядок
+    df = df[feature_columns]
 
-    feature_columns = ['OneHot__os_iOS', 'OneHot__source_organic', 'MeanTarget__country',
-       'MeanTarget__city', 'MeanTarget__topic', 'month',
-       'day', 'hour', 'user_id',
-       'post_id', 'gender', 'age',
-       'exp_group', 'mean_tfidf_like_posts',
-       'lenght_post', 'tfidf_mean',
-       'tfidf_max', 'svd_column',
-       'rbf_centr_1', 'rbf_centr_2',
-       'rbf_centr_3', 'rbf_centr_4',
-       'rbf_centr_5', 'rbf_centr_6',
-       'rbf_centr_7', 'rbf_centr_8',
-       'rbf_centr_9', 'rbf_centr_10',
-       'favorite_topic']
-
-    df = df.reindex(columns=feature_columns)
-
-    # TODO: получите предсказания вероятности лайка от модели
+    # Получаем предсказания в виде вероятностей
     df['prediction'] = model.predict_proba(df)[:, 1]
 
-    # TODO: выберите top-N постов с наибольшей вероятностью
+    # Формируем список limit постов, где самая высокая вероятность того, что пользователю понравится пост.
     top_post_idx = df.nlargest(limit, 'prediction')['post_id'].tolist()
 
-
-    # TODO: сформируйте список объектов PostGet для ответа сервиса
+    # Получаем рекомендованные посты в обычном виде
     recs_posts = post_info[post_info['post_id'].isin(top_post_idx)]
+
     recs = [
-        PostGet(
-            id=item['post_id'],
-            text=item['text'],
-            topic=item['topic']
-        )
-        for _, item in recs_posts.iterrows()
+        PostGet(id=item.post_id, text=item.text, topic=item.topic)
+        for item in recs_posts.itertuples()
     ]
 
     return recs
